d288919814f7e36fee4fa6e413a59c0df4348fb6
Victor Godoy <victor.hgodoy@icloud.com>
2025-09-15 21:25:56 -0300

    fix: gitignore

--- app/scripts/test_ncm_flow.py
+++ app/scripts/test_ncm_flow.py
@@ -3,9 +3,12 @@ from app.db.chroma_db.chromadb_manager import chroma_manager
 from app.db.chroma_db.model import Response
 
 fake_descriptions = [
-    "Aluminum Electrolytic Capacitor, 10 uF, 100 V, ± 20%, 2000 hours @ 85°C, Radial Can - SMD", #8532.22.00 
-    "Cap Ceramic 1uF 16V X7R 5% Pad SMD 0805 125°C T/R", #8532.24.10 ou #8532.25.10  8532.30.10 
-    "Rectifier Bridge Diode Single 1KV 2A 4-Pin Case KBP Box", #8541.10 
+    "Capacitor eletrolítico de alumínio, 10 uF, 100 V, ±20%, 2000 horas a 85°C, Radial Can - SMD",  # 8532.22.00
+    "Capacitor cerâmico 1 uF, 16 V, X7R, 5%, encapsulamento 0805, até 125°C, montagem SMD (tape & reel)",  # 8532.24.10 ou 8532.25.10 / 8532.30.10
+    "Diodo retificador em ponte, 1 kV, 2 A, 4 pinos, encapsulamento KBP, formato caixa",  
+    "Transistor MOSFET canal N, 200 V, 18 A, resistência 0.15 Ω, encapsulamento TO-263AB, montagem em superfície",  
+    "Resistor de filme metálico, 10 kΩ, 1/4 W, tolerância ±1%, encapsulamento axial, uso geral",  
+    "Transformador de potência, 500 VA, frequência 50/60 Hz, primário 220 V, secundário 24 V, encapsulado",  
 ]
 
 fetch_ncm_data()
