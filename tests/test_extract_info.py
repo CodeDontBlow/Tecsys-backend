62b926a02d683c90af54fdb3d38aeedef1047325
leonardo-dsouza <dkaireturn@gmail.com>
2025-09-22 20:58:53 -0300

    refactor: add classe e melhorando a importação

--- tests/test_extract_info.py
+++ tests/test_extract_info.py
@@ -1,9 +1,10 @@
-from app.scripts.extract import company_name, return_Datajson
+from app.scripts.extract_json import Extract_json
+from app.scripts.find_info import Find_info
 from app.scripts.pdf2txt import pdf_to_text_native
 
 texto = pdf_to_text_native("exemplo_pdf_entrada.pdf")
 print(texto)
-data_json = return_Datajson(texto)
+data_json = Extract_json.return_Datajson(texto)
 
 print(data_json)
-print(company_name(texto))
+print(Find_info.find_company_name(texto))
