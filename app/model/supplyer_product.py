89edb81f1c8c987098249b7268b43cb7feae2945
YgorPereira <ygorrp25@gmail.com>
2025-09-22 21:08:58 -0300

    #CDB-54/fix-missing-supplyer_products-model-column

--- app/model/supplyer_product.py
+++ app/model/supplyer_product.py
@@ -11,6 +11,7 @@ class SupplyerProduct(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     supplyer_id: Mapped[int] = mapped_column(Integer, ForeignKey("suppliers.id"), nullable=False)
     product_ncm: Mapped[str] = mapped_column(String(10), ForeignKey("products.ncm"), nullable=False)
+    erp_description: Mapped[str] = mapped_column(String(255), nullable=True)
 
     supplyer: Mapped["Supplyer"] = relationship("Supplyer", back_populates="supplyer_products")
     product: Mapped["Product"] = relationship("Product", back_populates="supplyer_products")
