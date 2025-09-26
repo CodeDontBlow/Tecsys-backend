eb8c6f3b57726e0d4f108059e9a5bd2cc7599e51
leonardo-dsouza <dkaireturn@gmail.com>
2025-09-26 07:44:28 -0300

    hotfix: type fix of method clean response

--- app/services/ollama_service/clean_response.py
+++ app/services/ollama_service/clean_response.py
@@ -2,7 +2,7 @@ import re
 class Clean_response:
 
     @staticmethod
-    def _clean_response(response_text):
+    def clean_response(response_text):
        cleaned_text = re.sub(r'<think>.*?</think>', '', response_text, flags=re.DOTALL)
        cleaned_text = re.sub(r'<think>.*', '', cleaned_text, flags=re.DOTALL)
        cleaned_text = re.sub(r'\n\s*\n', '\n\n', cleaned_text.strip())
