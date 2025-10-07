870994a34d20a8006e78120c7d3649546304c2d0
leonardo-dsouza <dkaireturn@gmail.com>
2025-10-07 10:17:14 -0300

    fix: postgresql assync dependence for docker

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,13 @@
-from pydantic_settings import BaseSettings
-
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "tecsys-backend"
     ROOT_PATH: str = '/'
     DATABASE_URL: str
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
-
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
 
 settings = Settings()
