636da3a88434ca0c7183a8e2b12e2eef9b7dca6b
henrySilverIX <henrysilver.gold@proton.me>
2025-10-09 21:06:21 -0300

    primeiro commit

--- app/core/dependencies.py
+++
@@ -1,7 +0,0 @@
-from typing import Annotated
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app.db.database import get_session
-
-DatabaseDependency = Annotated[AsyncSession, Depends(get_session)]
