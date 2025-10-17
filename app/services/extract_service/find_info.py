c173269e7103aa110f810339dfaa29951e77e9ed
leonardo-dsouza <dkaireturn@gmail.com>
2025-10-16 23:35:10 -0300

    fix: extract new pdf

--- app/services/extract_service/find_info.py
+++ app/services/extract_service/find_info.py
@@ -14,48 +14,51 @@ class Find_info:
     def find_product_info(linha):
         if re.match(r'^\d{2}\s+', linha.strip()):
             partes = linha.split()
+
+            mercadoria = partes[0]  
+
+            padrão_erp = r'^\d{9}$'
+            for parte in partes:
+                parte_limpa = str(parte).strip()
+                if re.match(padrão_erp, parte_limpa):
+                    codigo_erp = parte_limpa
             
-            if len(partes) >= 10:
-                mercadoria = partes[0]  
-                codigo_erp = partes[6]  
+        
+            desc_start = linha.find('-')
+            if desc_start != -1:
+                descricao = linha[desc_start + 1:].strip()
+                part_number_match = re.search(r'PN:(\S+)', descricao)
+                if part_number_match:
+                    part_number = part_number_match.group(1) 
+                    descricao = descricao.split()
+
+
+                    for index, desc in enumerate(descricao):
+                        if 'PN:' in desc:
+                            descricao.pop(index)
+                            
+                    descricao = ' '.join(descricao)
+
+                elif part_number_match == None:
+                    descricao = descricao.split()
+                    part_number = descricao[0]
+                    descricao.pop(0)
+                    descricao = ' '.join(descricao)
+
+                else:
+                    return None
+                    
                 
-            
-                desc_start = linha.find('-')
-                if desc_start != -1:
-                    descricao = linha[desc_start + 1:].strip()
-                    
-                    part_number_match = re.search(r'PN:(\S+)', descricao)
-                    if part_number_match:
-                        part_number = part_number_match.group(1) 
-                        descricao = descricao.split()
-
-
-                        for index, desc in enumerate(descricao):
-                            if 'PN:' in desc:
-                                descricao.pop(index)
-                                
-                        descricao = ' '.join(descricao)
-
-                    elif part_number_match == None:
-                        descricao = descricao.split()
-                        part_number = descricao[0]
-                        descricao.pop(0)
-                        descricao = ' '.join(descricao)
-
-                    else:
-                        return None
-                        
-                    
-                    
-                    if 'PN:' in part_number:
-                        nome = descricao.split('PN:')[0].strip().rstrip('-').strip()
-                    else:
-                        nome = descricao.rstrip('-').strip()
-                    return {
-                        'mercadoria': mercadoria,
-                        'nome': nome,
-                        'part_number': part_number,
-                        'codigo_erp': codigo_erp
-                    }
+                
+                if 'PN:' in part_number:
+                    nome = descricao.split('PN:')[0].strip().rstrip('-').strip()
+                else:
+                    nome = descricao.rstrip('-').strip()
+                return {
+                    'mercadoria': mercadoria,
+                    'nome': nome,
+                    'part_number': part_number,
+                    'codigo_erp': codigo_erp
+                }
         
         return None
--- tests/test_extract_info.py
+++ tests/test_extract_info.py
@@ -2,7 +2,7 @@ from app.services.extract_service.enterPDF import EnterPDF
 
 # add the EnterPDF method
 
-pdf_processado1 = EnterPDF("exemplo_pdf_entrada.pdf")
+pdf_processado1 = EnterPDF("Pedido compras_Mouser.pdf")
 
 dados = pdf_processado1.process_enter()
 nome = pdf_processado1.get_supplier_name()
@@ -11,3 +11,12 @@ descs = pdf_processado1.get_erp_desc()
 pns = pdf_processado1.get_pn()
 codes = pdf_processado1.get_erp_code()
 
+
+print(nome)
+print("-" * 50)
+print(descs)
+print("-" * 50)
+print(pns)
+print("-" * 50)
+print(codes)
+print("-" * 50)
