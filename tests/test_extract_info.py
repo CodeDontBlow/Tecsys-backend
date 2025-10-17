0b4ff823318424c16c21443e2c5b4d398c90651f
leonardo-dsouza <dkaireturn@gmail.com>
2025-10-17 10:40:45 -0300

    refactor: fix and refactor all code extract_pdf

--- tests/test_extract_info.py
+++ tests/test_extract_info.py
@@ -2,7 +2,7 @@ from app.services.extract_service.enterPDF import EnterPDF
 
 # add the EnterPDF method
 
-pdf_processado1 = EnterPDF("exemplo_pdf_entrada.pdf")
+pdf_processado1 = EnterPDF("mouser.pdf")
 
 dados = pdf_processado1.process_enter()
 nome = pdf_processado1.get_supplier_name()
@@ -11,3 +11,4 @@ descs = pdf_processado1.get_erp_desc()
 pns = pdf_processado1.get_pn()
 codes = pdf_processado1.get_erp_code()
 
+print(dados)
