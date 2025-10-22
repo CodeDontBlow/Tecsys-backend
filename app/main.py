6514919e640375b962c1cff73ed82cd5cc2767c2
Matheus Pires <matheusdisabatino@outlook.com>
2025-10-22 15:31:57 -0300

    feat [CDB-60]: Websocket config

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, WebSocket, WebSocketDisconnect, UploadFile
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-
+from app.manager import WebSocketManager
+from app.tasks import process_pdf, process_scraping, process_ncm, process_llm
+import asyncio
 from app.core.config import settings
 from app.api.router_global import router
 
+ws_manager = WebSocketManager()
+task_queue = asyncio.Queue()
 
 class App(FastAPI):
     def __init__(self, *args, **kwargs) -> None:
@@ -26,3 +31,82 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(router, prefix="/api")
+
+
+# Websocket
+
+# Config
+
+# Função do worker que processa as tarefas
+async def worker():
+    while True:
+        task_func = await task_queue.get()
+        if task_func is None:
+            break
+        await task_func()
+        task_queue.task_done()
+
+# Função do worker que processa as tarefas
+async def worker():
+    while True:
+        task_func = await task_queue.get()
+        if task_func is None:
+            break
+        await task_func()
+        task_queue.task_done()
+
+# Função para adicionar tarefas à fila
+async def add_task_to_queue(task_func):
+    await task_queue.put(task_func)
+
+# Inicia worker
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(worker())
+
+# Rota WebSocket
+@app.websocket("/ws/notify")
+async def websocket_endpoint(websocket: WebSocket):
+    await ws_manager.connect(websocket)
+    try:
+        while True:
+            message = await websocket.receive_text()
+    except WebSocketDisconnect:
+        ws_manager.disconnect(websocket)
+        print("Cliente desconectado")
+
+# Função para enviar mensagem via WebSocket
+async def send_websocket_message(message: str):
+    if ws_manager.active_connections: 
+        await ws_manager.send_message(message)
+
+# Modelo para validar os dados da requisição
+class TaskRequest(BaseModel):
+    task_name: str
+    file: str = None
+    pn: str = None
+    ncm: str = None
+    text: str = None
+
+# Rota para iniciar tarefas
+@app.post("/api/v1/pdf/upload")
+async def upload_pdf(file: UploadFile = None, task_request: TaskRequest = None):
+    
+    await send_websocket_message("PDF Carregado")
+
+    await send_websocket_message("Pegando Informações")
+    await add_task_to_queue(lambda: process_pdf(file.filename))
+
+    await send_websocket_message("Pesquisando PN")
+    await add_task_to_queue(lambda: process_scraping(task_request.pn))
+
+    await send_websocket_message("Pesquisando Fabricante")
+    await send_websocket_message("Estimando NCM")
+    await add_task_to_queue(lambda: process_ncm(task_request.ncm))
+
+    await send_websocket_message("Gerando Descrição")
+    await add_task_to_queue(lambda: process_llm(task_request.text))
+
+    await send_websocket_message("Processamento Completo.")
+
+    return {"message": "PDF carregado e processo iniciado."}
---
+++ app/tasks.py
@@ -0,0 +1,26 @@
+from app.manager import WebSocketManager
+import asyncio
+from app.services.ncm_service import get_ncm
+
+# Instância do WebSocketManager para enviar notificações.
+ws_manager = WebSocketManager()
+
+async def process_pdf(pdf_file: str):
+    await ws_manager.send_message(f"Processando PDF: {pdf_file}")
+    await asyncio.sleep(3)  # tempo simulado de processamento
+    await ws_manager.send_message(f"PDF {pdf_file} extraído com sucesso!")
+
+async def process_scraping(pn: str):
+    await ws_manager.send_message(f"Iniciando scraping com o PN: {pn}")
+    await asyncio.sleep(5)  # tempo simulado do scraping
+    await ws_manager.send_message(f"Scraping com PN {pn} concluído!")
+
+async def process_ncm(ncm: str):
+    await ws_manager.send_message(f"Iniciando validação de NCM: {ncm}")
+    await get_ncm("LED Verde, 2 mm, 5,2 mcd, 560 nm, SMD, If 20 mA, Vf 2,1 V, Ângulo 130°, Lente Dome")
+    await ws_manager.send_message(f"NCM {ncm} validado com sucesso!")
+
+async def process_llm(text: str):
+    await ws_manager.send_message(f"Iniciando geração de descrição com IA para: {text}")
+    await asyncio.sleep(4)  # tempo simulado de processamento da IA
+    await ws_manager.send_message(f"Descrição gerada com sucesso para: {text}")
