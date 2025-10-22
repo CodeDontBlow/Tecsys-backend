f52d7c47a07fd89305942d3e650efe22d0404373
Leonardo Cristiano <163062294+Leonardo-dSouza@users.noreply.github.com>
2025-10-21 22:29:51 -0300

    Update PDF file name in test_extract_info.py

--- tests/test_extract_info.py
+++ tests/test_extract_info.py
@@ -2,7 +2,7 @@ from app.services.extract_service.enterPDF import EnterPDF
 
 # add the EnterPDF method
 
-pdf_processado1 = EnterPDF("mouser.pdf")
+pdf_processado1 = EnterPDF("exemplo_pdf_entrada.pdf")
 
 dados = pdf_processado1.process_enter()
 nome = pdf_processado1.get_supplier_name()
@@ -10,5 +10,3 @@ nome = pdf_processado1.get_supplier_name()
 descs = pdf_processado1.get_erp_desc()
 pns = pdf_processado1.get_pn()
 codes = pdf_processado1.get_erp_code()
-
-print(dados)
